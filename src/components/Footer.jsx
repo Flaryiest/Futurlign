f452e4597ec813a7325a645bd72f7b151de1e935
Flaryiest <ericmzuo1@gmail.com>
2024-08-06 19:49:17 -0600

    finished footer + misc changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,20 @@ import "/src/style/footer.css"
 
 function Footer() {
     return <div className="footer">
-        <div className="fade"></div>
-        <div className="footerLeft">Futurlign</div>
+        <div className="footerContent">
+            <div className="footerLeft">
+                <div className="footerHeader">Futurlign</div>
+                <div className="footerContentSmall">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed nihil voluptas nisi veniam?</div>
+            </div>
+            <div className="footerMiddle">
+                <div className="footerHeader">Connect With Us</div>
+                <div className="footerContentSmall">Lorem, ipsum dolor sit amet consectetur adipisicing elit. quam cupiditate reiciendis praesentium ratione optio omnis deleniti tempora fugiat?</div>
+            </div>
+            <div className="footerRight">
+                <div className="footerHeader">Web Design & Development By Eric Zuo at ericmzuo1@gmail.com</div>
+            </div>
+        </div>
+        <div className="fade marginBottom"></div>
     </div>
 }
 
