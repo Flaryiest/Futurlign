7a5d2650bb092cf556769a1497bd46e8c0f1ada6
Flaryiest <ericmzuo1@gmail.com>
2024-08-07 09:53:21 -0600

    finished info section + jsx for events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -2,8 +2,29 @@ import NavBar from "/src/components/Navbar.jsx"
 import Footer from "/src/components/Footer.jsx"
 import "/src/style/events.css"
 function Events() {
-    return <div className="eventsContent">
+    return <div className="events">
         <NavBar/>
+        <div className="eventsContent">
+            <h2 className="eventsHeader">Futurlign Aligns</h2>
+            <div className="eventsImages">
+                <img src="/src/assets/EventsImageOne" className="eventImage" alt="Image of Children Learning How AI Works"></img>
+                <img src="/src/assets/EventsImageTwo" className="eventImage" alt="Image of Children Learning How AI Works"></img>
+                <img src="/src/assets/EventsImageThree" className="eventImage" alt="Image of Children Learning How AI Works"></img>
+                <img src="/src/assets/EventsImageFour" className="eventImage" alt="Image of Children Learning How AI Works"></img>
+                <img src="/src/assets/EventsImageFive" className="eventImage" alt="Image of Children Learning How AI Works"></img>
+                <img src="/src/assets/EventsImageSix" className="eventImage" alt="Image of Children Learning How AI Works"></img>
+                <img src="/src/assets/EventsImageSeven" className="eventImage" alt="Image of Children Learning How AI Works"></img>
+                <img src="/src/assets/EventsImageEight" className="eventImage" alt="Image of Children Learning How AI Works"></img>
+            </div>
+            <div className="eventsSection">
+                <h2 className="eventsHeader">Upcoming Events</h2>
+                <div className="eventsText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus facilis in magni ab voluptates eligendi quia atque voluptatum ullam harum laboriosam deleniti, illum quos omnis explicabo facere ratione nostrum? Mollitia.</div>
+            </div>
+            <div className="eventsSection">
+                <h2 className="eventsHeader">Upcoming Events</h2>
+                <div className="eventsText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus facilis in magni ab voluptates eligendi quia atque voluptatum ullam harum laboriosam deleniti, illum quos omnis explicabo facere ratione nostrum? Mollitia.</div>
+            </div>
+        </div>
         <Footer/>
     </div>
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,8 +3,34 @@ import Footer from "/src/components/Footer.jsx"
 import "/src/style/info.css"
 
 function Info() {
-    return <div className="infoContent">
+    return <div className="info">
         <NavBar/>
+        <div className="infoTemp">
+            <div className="infoContent">
+                <div className="infoSection">
+                    <div className="infoSectionHeader">Core Principles:</div>
+                    <ul className="infoSectionList">
+                        <li className="infoSectionListItem">Lorem, ipsum dolor sit amet consectetur</li>
+                        <li className="infoSectionListItem">Lorem, ipsum dolor sit amet consectetur</li>
+                        <li className="infoSectionListItem">Lorem, ipsum dolor sit amet consectetur</li>
+                    </ul>
+                </div>
+                <div className="infoSection">
+                    <div className="infoSectionHeader">Motivation</div>
+                    <div className="infoSectionText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum veritatis ab eligendi placeat qui, ipsam unde fuga, reiciendis ducimus iusto quia. Porro, laborum molestiae. Ratione, reprehenderit quos. Praesentium, cupiditate adipisci! Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium aut ipsam corrupti neque quas, obcaecati repudiandae magni tempore sint doloribus, esse quidem eos! Doloribus, fugit. Enim blanditiis soluta perspiciatis quis?</div>
+                </div>
+                <div className="infoSection">
+                    <div className="infoSectionHeader">What</div>
+                    <div className="infoSectionText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum veritatis ab eligendi placeat qui, ipsam unde fuga, reiciendis ducimus iusto quia. Porro, laborum molestiae. Ratione, reprehenderit quos. Praesentium, cupiditate adipisci! Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quibusdam culpa dolore aperiam saepe deleniti officia quaerat veritatis at quia repellat adipisci, ullam, molestias ipsa voluptas cum tempora iste odio!</div>
+                </div>
+                <div className="infoSection">
+                    <div className="infoSectionHeader">Who</div>
+                    <div className="infoSectionText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum veritatis ab eligendi placeat qui, ipsam unde fuga, reiciendis ducimus iusto quia. Porro, laborum molestiae. Ratione, reprehenderit quos. Praesentium, cupiditate adipisci! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cum repellendus ratione explicabo ducimus perferendis sit. Nihil distinctio voluptas ut tenetur pariatur, earum repellat odio dignissimos quae molestias eos repudiandae?</div>
+                </div>
+            </div>    
+        </div>
+        <div className="glowInfoOne"></div>
+        <div className="glowInfoTwo"></div>
         <Footer/>
     </div>
 }
