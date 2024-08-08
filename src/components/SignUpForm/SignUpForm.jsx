5014743742f8916e36d9a6ace895ba42c9dad359
Flaryiest <ericmzuo1@gmail.com>
2024-08-07 20:15:41 -0600

    bugfixes + used react form hook on signup

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,41 +1,48 @@
-import "/src/style/signUpForm.css"
+
 import axios from "axios"
 import NavBar from "../Navbar"
 import Footer from "../Footer"
-import SkillSelector from "./SkillSelector"
+import ScrollToTop from "../ScrollToTop"
+import { useForm } from "react-hook-form"
+import "/src/style/signUpForm.css"
 function SignUpForm() {
+    const {register, handleSubmit, formState: {errors}} = useForm()
+    const onSubmit = (data) => {
+        console.log(data)
+    }
     return <div className="signUpForm">
+        <ScrollToTop/>
         <NavBar/>
         <div className="signUpFormContent">
             <h2 className="signUpHeader">Sign Up</h2>
-            <form>
-                <div class="card">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="card">
                     <div>
-                    <label class="input">
-                        <input class="input__field" type="text" name="fullName" placeholder=" " />
-                        <span class="input__label">Full Name</span>
-                    </label>
-                    <div class="divider"></div>
-                    <label class="input">
-                        <input class="input__field" type="text" name="school" placeholder=" " />
-                        <span class="input__label">School</span>
-                    </label>
-
+                        <label className="input">
+                            <input className="input__field" type="text" name="fullName" placeholder=" " {...register("fullName", { required: true })}/>
+                            <span className="input__label">Full Name</span>
+                        </label>
+                        <div className="divider"></div>
+                        <label className="input">
+                            <input className="input__field" type="text" name="school" placeholder=" " {...register("school", { required: true })}/>
+                            <span className="input__label">School</span>
+                        </label>
                     </div>
                     <div>
-                    <label class="input">
-                        <input class="input__field" type="text" name="email" placeholder=" " />
-                        <span class="input__label">Email</span>
+                    <label className="input">
+                        <input className="input__field" type="text" name="email" placeholder=" " {...register("email", { required: true })}/>
+                        <span className="input__label">Email</span>
                     </label>
-                    <div class="divider"></div>
-                    <label class="input">
-                        <input class="input__field" type="text" name="grade" placeholder=" " />
-                        <span class="input__label">Grade</span>
+                    <div className="divider"></div>
+                    <label className="input">
+                        <input className="input__field" type="text" name="grade" placeholder=" " {...register("grade", { required: true })}/>
+                        <span className="input__label">Grade</span>
                     </label>
                     </div>
+                    
                 </div>
-                <div class="button-group">
-                    <button class="button">Submit</button>
+                <div className="button-group">
+                    <button className="button">Submit</button>
                 </div>
             </form>
         </div>
