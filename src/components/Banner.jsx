be2de8df39690708978dd5d03cff1825b6cc1e8b
Flaryiest <ericmzuo1@gmail.com>
2024-12-16 17:27:02 -0700

    removed calgary library from supporters

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,6 @@ function Banner() {
     <h2 className="banner-header">Our Partners</h2>
     <div className="supporter-logos">
         <img className="supporter-logo" src="/assets/government-logo.png"></img>
-        <img className="supporter-logo" src="/assets/cpl-logo-two.png"></img>
         <img className="supporter-logo" src="/assets/ucalgary-logo.png"></img>
     </div>
     </div>
