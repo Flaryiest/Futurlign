b66ce3354420a71d19266d5340accdc5e804ae7a
Flaryiest <ericmzuo1@gmail.com>
2025-03-02 14:52:12 -0700

    created new page

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Events from './components/Events.jsx'
 import Network from './components/Network.jsx'
 import SignUpForm from './components/SignUpForm/SignUpForm.jsx'
 import Form from "./components/Form.jsx"
+import Course from "./components/Course.jsx"
 import "/src/style/App.css"
 import './style/index.css'
 
@@ -34,6 +35,10 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <Form/>
+  },
+  {
+    path: "/course",
+    element: <Course/>
   }
 ])
 
